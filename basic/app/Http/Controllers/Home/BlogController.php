5ce0f107a52b41340d6926e5542e223402234ce5
ahmadothman1981 <ahmadothman@outlook.com>
2022-09-21 16:16:41 +0200

    add blog

--- basic/app/Http/Controllers/Home/BlogController.php
+++ basic/app/Http/Controllers/Home/BlogController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Home;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Blog;
+use App\Models\BlogCategory;
 use Image;
 use Illuminate\Support\Carbon;
 
@@ -16,5 +17,31 @@ class BlogController extends Controller
         $blogs = Blog::latest()->get();
 
         return view('admin.blogs.blogs_all',compact('blogs'));
+    }//End Method
+
+    public function AddBlog()
+    {
+        $categories = BlogCategory::orderBy('blog_category','ASC')->get();
+        return view('admin.blogs.blogs_add',compact('categories'));
+    }//End Method
+
+    public function StoreBlog(Request $request)
+    {
+        $image = $request->file('blog_image');
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            Image::make($image)->resize(430,327)->save('upload/blog/'.$name_gen);
+            $save_url = 'upload/blog/'.$name_gen;
+            Blog::insert([
+                'blog_category_id'=> $request->blog_category_id,
+                'blog_title'=>$request->blog_title,
+                'blog_tags'=>$request->blog_tags,
+                'blog_description' =>  $request->blog_description,
+               'blog_image' => $save_url,
+               'created_at'=>Carbon::now(),
+            ]);
+
+        session()->flash('message','Blog inserted  Successfully');
+
+        return redirect()->route('all.blog');
     }//End Method
 }
--- basic/resources/views/admin/admin_master.blade.php
+++ basic/resources/views/admin/admin_master.blade.php
@@ -30,6 +30,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/bootstrap.tagsinput/0.8.0/bootstrap-tagsinput.css" >
 
            
 
@@ -147,7 +148,7 @@
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
         <script src="{{ asset('backend/assets/js/code.js') }}"></script>
-
+        <script src="https://cdn.jsdelivr.net/bootstrap.tagsinput/0.8.0/bootstrap-tagsinput.min.js" ></script>
 
     </body>
 
